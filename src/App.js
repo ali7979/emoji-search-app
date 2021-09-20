accbb626d48308324e1bf0e79b8190fad41d0291
Zoheb Alam <zohebzob@gmail.com>
2021-09-20 16:37:25 +0530

    commit

--- src/App.js
+++ src/App.js
@@ -1,25 +1,65 @@
-import logo from './logo.svg';
+import React, { useState } from 'react';
+import Picker from 'emoji-picker-react';
 import './App.css';
+import {CopyToClipboard} from 'react-copy-to-clipboard'
+const App = () => {
+  const [chosenEmoji, setChosenEmoji] = useState(null);
 
-function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+  const onEmojiClick = (event, emojiObject) => {
+    setChosenEmoji(emojiObject);
+
+    
+
+    
+  };
+  
+  async function f(){
+  var edt=await document.getElementsByTagName("input");
+
+  edt[0].placeholder="Search for your EMOJI"
+
+}
+f();
+
+ function al(){
+  alert("Emoji Copied to Clipboard");
 }
 
+
+
+  return (
+    
+    <><div className="box">
+
+      <Picker onEmojiClick={onEmojiClick} />
+
+    </div><div className="txt">
+        {chosenEmoji ? (
+          <span>You chose: {chosenEmoji.emoji}</span>
+        ) : (
+          <span>Emoji Search App</span>
+        )}
+       
+
+
+
+       <CopyToClipboard text={chosenEmoji?chosenEmoji.emoji:"a"}>
+  <button className="cpy" onClick={al}> <i className="fa fa-clipboard" aria-hidden="true"></i></button>
+</CopyToClipboard>
+
+
+
+
+
+      </div>
+      
+      
+      
+      </>
+
+
+          
+
+  );
+};
 export default App;
